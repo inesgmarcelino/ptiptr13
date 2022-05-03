391af8e4a2cdcde269398b96e04bcd95a4d4cc0b
inesgmarcelino <inesgmarcelino@gmail.com>
2022-05-03 20:11:43 +0100

    css stylying

--- ecomarket-app/src/components/Login.jsx
+++ ecomarket-app/src/components/Login.jsx
@@ -59,41 +59,75 @@ function Login() {
     }
 
     return (
-        <div className="col-8 justify-content-center mt-5">
-            <div class="form-holder">
-                <div class="form-content position-absolute">
-                    <div class="form-items">
-                        <h3>LOGIN</h3>
-                        <p>Inicie sessão aqui.</p>
+        // <div className="col-8 justify-content-center mt-5">
+        //     <div className="form-holder">
+        //         <div className="form-content position-absolute">
+        //             <div className="form-items">
+        //                 <h3>LOGIN</h3>
+        //                 <p>Inicie sessão aqui.</p>
 
-                        <form method="get">
-                            <div class="col-md-12">
-                                <input class="form-control" type="email" name="email" placeholder="Email" onChange={handler} required />
-                            </div>
-                            <div class="col-md-12">
-                                <input class="form-control" type="password" name="password" placeholder="Password" onChange={handler} required />
-                            </div>
+        //                 <form method="get">
+        //                     <div className="col-md-12">
+        //                         <input className="form-control" type="email" name="email" placeholder="Email" onChange={handler} required />
+        //                     </div>
+        //                     <div className="col-md-12">
+        //                         <input className="form-control" type="password" name="password" placeholder="Password" onChange={handler} required />
+        //                     </div>
                             
-                            <button id="submit" type="submit" name="submit" class="btn" onClick={handler}>Iniciar Sessão</button>
-                        </form>
-                    </div>
+        //                     <button id="submit" type="submit" name="submit" className="btn" onClick={handler}>Iniciar Sessão</button>
+        //                 </form>
+        //             </div>
+        //         </div>
+        //     </div>
+        //     {/* MODAL */}
+        //     <div className="modal fade" id="modal_login" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        //         <div className="modal-dialog modal-dialog-centered">
+        //             <div className="modal-content">
+        //                 <div className="modal-header" id="modal_header_login">~
+        //                     <button type="button" className="btn-close" aria-label="Close"></button>
+        //                 </div>
+        //                 <div className="modal-body" id="modal_body_login">
+        //                 </div>
+        //                 <div className="modal-footer" id="modal_footer_login">
+        //                 <button type="button" onClick={handleHide} className="btn" id="cancelar">Cancelar</button>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+        <div>
+            <div className="card position-absolute top-50 start-50 translate-middle">
+                <div className="card-body">
+                    <h5 className="card-title">LOGIN</h5>
+                    <h6 className="card-subtitle mb-2">Inicie aqui a Sessão</h6>
+                    <form method="get">
+                        <div className="col-md-12">
+                            <input className="form-control" type="email" name="email" placeholder="Email" onChange={handler} required />
+                        </div>
+                        <div className="col-md-12">
+                            <input className="form-control" type="password" name="password" placeholder="Password" onChange={handler} required />
+                        </div>
+                    
+                        <button id="submit" type="submit" name="submit" className="btn" onClick={handler}>Iniciar Sessão</button>
+                    </form>
                 </div>
             </div>
+
             {/* MODAL */}
-            <div className="modal fade" id="modal_login" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered">
-                    <div className="modal-content">
-                        <div className="modal-header" id="modal_header_login">~
-                            <button type="button" class="btn-close" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body" id="modal_body_login">
-                        </div>
-                        <div className="modal-footer" id="modal_footer_login">
-                        <button type="button" onClick={handleHide} className="btn" id="cancelar">Cancelar</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+             <div className="modal fade" id="modal_login" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                 <div className="modal-dialog modal-dialog-centered">
+                     <div className="modal-content">
+                         <div className="modal-header" id="modal_header_login">~
+                             <button type="button" className="btn-close" aria-label="Close"></button>
+                         </div>
+                         <div className="modal-body" id="modal_body_login">
+                         </div>
+                         <div className="modal-footer" id="modal_footer_login">
+                         <button type="button" onClick={handleHide} className="btn" id="cancelar">Cancelar</button>
+                         </div>
+                     </div>
+                 </div>
+             </div>
         </div>
     );
 }
--- ecomarket-app/src/index.js
+++ ecomarket-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {SideBar, Home, Login, Register, Cart } from './components';
+import {SideBar, Home, Login, Register, Cart, Footer } from './components';
 
 ReactDOM.render(
   <Router>
@@ -13,6 +13,7 @@ ReactDOM.render(
       <Route path='/register' element={<Register />} />
       <Route path='/cart' element={<Cart />} />
     </Routes>
+    <Footer />
   </Router>,
 
   document.getElementById('root')
