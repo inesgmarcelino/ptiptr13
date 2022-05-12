db85397681e0afde80aad39b19c2ab643eabaa1d
fc51601 <fc51601@alunos.fc.ul.pt>
2022-05-12 03:40:17 +0100

    supporte pra auth0 semi feito

--- eco-market-backend/routes/api/v1/users.js
+++ eco-market-backend/routes/api/v1/users.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 // https://stackoverflow.com/questions/62134713/nodejs-mysql-connection-best-practice
 // https://mhagemann.medium.com/create-a-mysql-database-middleware-with-node-js-8-and-async-await-6984a09d49f4
-var db = require('./db_lib/getPool');
-//Atencao esta route esta muito em pseudo codigo
+var db = require('./svlib/db/getPool');
+
+
+/** auth0 */
+var auth = require('./svlib/auth0/tokenlib');
+/** */
 
 //feel free para mudares a localizacao 
 // do connection.js
