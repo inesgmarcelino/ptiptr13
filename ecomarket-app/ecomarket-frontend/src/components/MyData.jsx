103411374a9ddfbff4cb94916985c7971acc6cb0
Sofia <sofiapedrolourenco@gmail.com>
2022-05-27 00:02:52 +0100

    Novo frontend MyData

--- ecomarket-app/ecomarket-frontend/src/components/MyData.jsx
+++ ecomarket-app/ecomarket-frontend/src/components/MyData.jsx
@@ -41,46 +41,42 @@ function MyData () {
     } */
 
     return(
-        <div className="position-absolute showItems" style={{position:'relative', display:'flex'}}>´
-            <div className="container">
-                <Link to ='/profile' ><button>Voltar</button></Link>
-                <br />
-                <h3>Está a editar os seus dados.</h3>     
-            <br />
-            <div className="container" style={{textAlign : 'center'}}>
-                <form method="post" style={{display: 'inline-block'}}>
-                        <label>NOME : 
-                            <input className="form-control" type="text" name="nome" placeholder="Nome" /* onChange={handler} required */ />
-                        </label>
-                    <div className="col-md-12">
-                        <label>
-                            EMAIL : 
-                            <input className="form-control" type="email" name="email" placeholder="Email" />
-                        </label>
-                    </div>
-                    <div className="col-md-12">
-                        <label>
-                            TELEMOVEL :
-                            <input className="form-control" type="text" name="telemovel" placeholder="Telemovel" />
-                        </label>
-                    </div>
-                    <div className="col-md-12">
-                        <label>
-                            PALAVRA-PASSE :
-                            <input className="form-control" type="password" name="palavra-chave" placeholder="Palavra-chave" />
-                        </label>
-                    </div>
-                    <br />
-                    <div className="col-md-12">
-                        <button type="button"  style={{display:'inline-block', justifyContent: 'center', alignItems: 'center', backgroundColor : 'green'}} >SALVAR</button>
-                    </div>
-                </form>
-            </div>
-            <div className='container'>
-                <button type="button" style={{alignItems:'right', backgroundColor:'red', textAlign : 'right', justifyContent:'right'}} >Remover conta</button>
-            </div>
+        <div>
+            <div className="card position-absolute top-50 start-50 translate-middle">
+                <div className="card-body">
+                    <h5 className="card-title">Editar Dados</h5>
+                    <h6 className="card-subtitle mb-2">Altere aqui os dados</h6>
+                    <form method="post">
+                             <div className="col-md-12">
+                                 <input className="form-control" type="text" name="nome" placeholder="Nome Completo"  />
+                             </div>
+                             <div className="col-md-12">
+                                 <input className="form-control" type="email" name="email" placeholder="Email"  />
+                             </div>
+                             <div className="col-md-12">
+                                 <input className="form-control" type="text" name="nif" placeholder="NIF" />
+                             </div>
+                             <div className="col-md-12">
+                                 <input className="form-control" type="text" name="telem" placeholder="Número de Telemóvel" />
+                             </div>
+                             <div className="col-md-12">
+                                 <input className="form-control" type="password" name="password" placeholder="Password"/>
+                             </div>
+                             <div className="col-md-12">
+                                 <input className="form-control" type="password" name="checkPassword" placeholder="Confirme a Password" />
+                             </div>
+                             
+                            
+                             <button id="submit" type="submit" name="submit" className="btn">Guardar</button>
+                         </form>
+                </div>
             </div>
         </div>
+    
+                
+            
+        
+        
     )
 }
 
