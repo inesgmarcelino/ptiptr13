dce746a28097a4d315bb36c6b75ab29216cd4ac3
Sebastiao <sebastiaocondeoliveira@gmail.com>
2022-05-28 19:13:28 +0100

    pagina do login do Admin criada

--- ecomarket-app/ecomarket-frontend/src/components/Admin.jsx
+++ ecomarket-app/ecomarket-frontend/src/components/Admin.jsx
@@ -1,21 +1,17 @@
 import React from 'react'
 
-function Consumidor () {
+function Admin() {
 
     return(
         <div>
             <div className="container">
                 <br />
-                <h3>As minhas encomendas</h3>     
-                <h4>Morada de entrega: </h4> {/* Aqui ir buscar a morada do consumidor */}
-                <button>ALTERAR</button>
+                <h3>Todos os utilizadores registados:</h3>     
             </div>
 
         </div>
-    )
-
-
+    );
 }
 
-export default Consumidor;
+export default Admin;
 
