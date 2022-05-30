f9a39e1973885befa1b3a5eb502258cc8dcb3b38
inesgmarcelino <inesgmarcelino@gmail.com>
2022-05-30 15:40:30 +0100

    trying db connection again

--- ecomarket-app/ecomarket-backend/routes/api/svlib/db/connection.js
+++ ecomarket-app/ecomarket-backend/routes/api/svlib/db/connection.js
@@ -8,15 +8,15 @@ var conn = mysql.createConnection({
   database: "ecodb"
 });
   
-  conn.connect(function(err) {
-    //if (err) throw err;
-    //comentado so para ser possivel correr, deopis corrige se quiseres
-
-    if(err){
-        console.log("could not connect :(");
-    } else {
-        console.log("Connected!");
-    }
+conn.connect(err => {
+  //if (err) throw err;
+  //comentado so para ser possivel correr, deopis corrige se quiseres
+  
+      if(err){
+          console.log("could not connect :(");
+      } else {
+          console.log("Connected!");
+      }
   });
 
 exports.conn = conn;
