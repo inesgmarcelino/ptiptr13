b31323f0211655172bcf494cf60d4ed5b4f23140
inesgmarcelino <inesgmarcelino@gmail.com>
2022-06-19 00:11:32 +0100

    delete useless stuff and fixing others -- not important changes

--- ecomarket-app/ecomarket-frontend/src/index.js
+++ ecomarket-app/ecomarket-frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import Auth0ProviderWithHistory from './auth/auth0-hist-prov';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {NavBar, Home, Login, Register, Cart, Footer, Profile, EditProfile, Catalog, Payment, ShoppingCart, Consumidor, 
-   Fornecedor, ConfirmarEncomenda, Comparador, Product, ArmazemRegister, ProductRegister, Album, AdminLogin, AdminTipos} from './components';
+   Fornecedor, ConfirmarEncomenda, Comparador, Product, StorageRegister, ProductRegister, AdminLogin, AdminTipos} from './components';
 
 
 const root = createRoot(
@@ -19,20 +19,18 @@ root.render(
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
       <Route path='/cart' element={<Cart />} />
-      <Route path='/catalog' element={<Catalog/>} />  
       <Route path='/profile' element={<Profile />} />
       <Route path='/editProfile' element={<EditProfile />} />
       <Route path='/payment' element={<Payment />} />
-      <Route path='/shoppingCart' element={<ShoppingCart />} />
       <Route path='/consumidor' element={<Consumidor />} />
       <Route path='/fornecedor' element={<Fornecedor />} />
       <Route path='/confirmarEncomenda' element={<ConfirmarEncomenda />} />
       <Route path='/comparador' element={<Comparador />} />
       <Route path='/adminLogin' element={<AdminLogin />} />
       <Route path='/product' element={<Product />} />
-      <Route path='/ArmazemRegister' element={<ArmazemRegister />} />
-      <Route path='/ProductRegister' element={<ProductRegister />} />
-      <Route path='/Album' element={<Album />} />
+      <Route path='/storageRegister' element={<StorageRegister />} />
+      <Route path='/productRegister' element={<ProductRegister />} />
+      <Route path='/catalog' element={<Catalog />} />
       <Route path='/admintipos' element={<AdminTipos />} />
 
 
