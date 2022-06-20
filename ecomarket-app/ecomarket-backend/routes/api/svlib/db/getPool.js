b2e29d8950d22e393fd7c675fcb692e00c568cb6
fc51601 <fc51601@alunos.fc.ul.pt>
2022-06-20 21:57:03 +0100

    fixes pra funcionar a testar com containers

--- ecomarket-app/ecomarket-backend/routes/api/svlib/db/getPool.js
+++ ecomarket-app/ecomarket-backend/routes/api/svlib/db/getPool.js
@@ -12,7 +12,8 @@ var mysql = require('mysql2');
 
 var pool = mysql.createPool({
     connectionLimit:10,
-    host: "10.0.0.5",
+    //host: "10.0.0.5",
+    host: "mysql",
     user: "root",
     password: "S3cret",
     database: "ecodb",
