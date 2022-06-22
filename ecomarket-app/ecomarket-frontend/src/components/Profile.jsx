1d1e231a5c54a49c7babf2d68ffdd9c9199cb582
inesgmarcelino <inesgmarcelino@gmail.com>
2022-06-22 15:49:07 +0100

    handler admin regist a new product type

--- ecomarket-app/ecomarket-frontend/src/components/Profile.jsx
+++ ecomarket-app/ecomarket-frontend/src/components/Profile.jsx
@@ -49,10 +49,8 @@ function Profile() {
                     <button type="button" className="btn-close" aria-label="Close"></button>
                 </div>
                 <div className="modal-body" id="modal_body_admin">
-                  <form method='post'>
-                    <div className='col-md-12' id="formulario"></div>
-                    <button id='submit' type='submit' name='submit' onSubmit={handler} className='btn'>Adicionar</button>
-                  </form>
+                  <div className='col-md-12' id="formulario"></div>
+                  <button id='submit' type='submit' name='submit' onSubmit={handler} className='btn'>Adicionar</button>
                 </div>
                 <div className="modal-footer" id="modal_footer_admin">
                   <button type="button" onClick={handleHide} className="btn" id="cancelar">Cancelar</button>
