e10466114d57e74b43d54be801877bd19cae9aae
inesgmarcelino <inesgmarcelino@gmail.com>
2022-06-22 21:43:13 +0100

    handler provider adds a new product

--- ecomarket-app/ecomarket-backend/routes/api/v1/providers.js
+++ ecomarket-app/ecomarket-backend/routes/api/v1/providers.js
@@ -101,139 +101,7 @@ router.post('/reg_product', (req,res) => {
     const tipo = req.body.tipo;
     const subtipo = req.body.subtipo;
     queryString = "INSERT INTO produto (nome, fornecedor, producao, preco, tipo, subtipo) VALUES (?,?,?,?,?,?,?)";
-    conn.query(queryString, [nome, prov, dataprod, preco, tipo, subtipo, idcad], (err, result) => {
-        conn.release();
-        
-        if(!err){
-            res.status(200);
-            res.type('json');
-            res.send({"message":"Registo bem sucessido"});
-            return;
-        } else {
-            res.status(500);
-            res.type('json');
-            res.send({"message":"Não foi possível realizar essa operação. output 15"});
-            return;
-        }
-    });
-    
     pool.getConnection((err, conn) => {
-        if (err) throw err;
-
-        conn.query(queryString, [nomerec, medida, quant], (err, result) => {
-            if (err) {
-                conn.release();
-
-                res.status(500);
-                res.type('json');
-                res.send({"message":"Não foi possível realizar essa operação. output 7"});
-                return;
-            }
-        });
-    
-        var idrec;
-        queryString = "SELECT id FROM recurso";
-        conn.query(queryString, [], (err,results) => {
-            if (!err) {
-                idcad = results[results.length -1].id; //por verificar
-            } else {
-                conn.release();
-                
-                res.status(500);
-                res.type('json');
-                res.send({"message":"Não foi possível realizar essa operação. output 8"});
-                return;
-            }
-        });
-    
-        const pol = req.body.pol;
-        queryString = "INSERT INTO poluicao (nome) VALUES (?)";
-        conn.query(queryString, [pol], (err, result) => {
-            if (err) {
-                conn.release();
-
-                res.status(500);
-                res.type('json');
-                res.send({"message":"Não foi possível realizar essa operação. output 9"});
-                return;
-            }
-        });
-    
-        var idpol;
-        queryString = "SELECT id FROM poluicao";
-        conn.query(queryString, [], (err,results) => {
-            if (!err) {
-                idpol = results[results.length -1].id; //por verificar
-            } else {
-                conn.release();
-                
-                res.status(500);
-                res.type('json');
-                res.send({"message":"Não foi possível realizar essa operação. output 10"});
-                return;
-            }
-        });
-    
-        queryString = "INSERT INTO cadeia_logistica";
-        conn.query(queryString, [], (err,result) => {
-            if (err) {
-                conn.release();
-
-                res.status(500);
-                res.type('json');
-                res.send({"message":"Não foi possível realizar essa operação. output 11"});
-                return;
-            }
-        });
-    
-        var idcad;
-        queryString = "SELECT id FROM cadeia_logistica";
-        conn.query(queryString, [], (err,results) => {
-            if (!err) {
-                idcad = results[results.length -1].id; //por verificar
-            } else {
-                conn.release();
-                
-                res.status(500);
-                res.type('json');
-                res.send({"message":"Não foi possível realizar essa operação. output 12"});
-                return;
-            }
-        });
-    
-        queryString = "INSERT INTO lista_recursos (cadeia_logis, recurso) VALUES (?,?)";
-        conn.query(queryString, [idcad, idrec], (err,result) => {
-            if (err) {
-                conn.release();
-
-                res.status(500);
-                res.type('json');
-                res.send({"message":"Não foi possível realizar essa operação. output 13"});
-                return;
-            }
-        });
-    
-        const quantpol = req.body.qtpol;
-        queryString = "INSERT INTO poluicao_cadeia (cadeia_logis, poluicao, quantidade) VALUES (?,?,?)";
-        conn.query(queryString, [idcad, idpol, quantpol], (err,result) => {
-            if (err) {
-                conn.release();
-
-                res.status(500);
-                res.type('json');
-                res.send({"message":"Não foi possível realizar essa operação. output 14"});
-                return;
-            }
-        });
-    
-        const prov = req.body.id;
-        const nome = req.body.nome;
-        const dataprod = req.body.dataprod;
-        const preco = req.body.preco;
-        const tipo = req.body.tipo;
-        const subtipo = req.body.subtipo;
-        queryString = "INSERT INTO produto (nome, fornecedor, producao, preco, tipo, subtipo, cadeia_logis) \
-                                    VALUES (?,?,?,?,?,?,?)";
         conn.query(queryString, [nome, prov, dataprod, preco, tipo, subtipo, idcad], (err, result) => {
             conn.release();
             
@@ -250,6 +118,140 @@ router.post('/reg_product', (req,res) => {
             }
         });
     });
+    
+    // pool.getConnection((err, conn) => {
+    //     if (err) throw err;
+
+    //     conn.query(queryString, [nomerec, medida, quant], (err, result) => {
+    //         if (err) {
+    //             conn.release();
+
+    //             res.status(500);
+    //             res.type('json');
+    //             res.send({"message":"Não foi possível realizar essa operação. output 7"});
+    //             return;
+    //         }
+    //     });
+    
+    //     var idrec;
+    //     queryString = "SELECT id FROM recurso";
+    //     conn.query(queryString, [], (err,results) => {
+    //         if (!err) {
+    //             idcad = results[results.length -1].id; //por verificar
+    //         } else {
+    //             conn.release();
+                
+    //             res.status(500);
+    //             res.type('json');
+    //             res.send({"message":"Não foi possível realizar essa operação. output 8"});
+    //             return;
+    //         }
+    //     });
+    
+    //     const pol = req.body.pol;
+    //     queryString = "INSERT INTO poluicao (nome) VALUES (?)";
+    //     conn.query(queryString, [pol], (err, result) => {
+    //         if (err) {
+    //             conn.release();
+
+    //             res.status(500);
+    //             res.type('json');
+    //             res.send({"message":"Não foi possível realizar essa operação. output 9"});
+    //             return;
+    //         }
+    //     });
+    
+    //     var idpol;
+    //     queryString = "SELECT id FROM poluicao";
+    //     conn.query(queryString, [], (err,results) => {
+    //         if (!err) {
+    //             idpol = results[results.length -1].id; //por verificar
+    //         } else {
+    //             conn.release();
+                
+    //             res.status(500);
+    //             res.type('json');
+    //             res.send({"message":"Não foi possível realizar essa operação. output 10"});
+    //             return;
+    //         }
+    //     });
+    
+    //     queryString = "INSERT INTO cadeia_logistica";
+    //     conn.query(queryString, [], (err,result) => {
+    //         if (err) {
+    //             conn.release();
+
+    //             res.status(500);
+    //             res.type('json');
+    //             res.send({"message":"Não foi possível realizar essa operação. output 11"});
+    //             return;
+    //         }
+    //     });
+    
+    //     var idcad;
+    //     queryString = "SELECT id FROM cadeia_logistica";
+    //     conn.query(queryString, [], (err,results) => {
+    //         if (!err) {
+    //             idcad = results[results.length -1].id; //por verificar
+    //         } else {
+    //             conn.release();
+                
+    //             res.status(500);
+    //             res.type('json');
+    //             res.send({"message":"Não foi possível realizar essa operação. output 12"});
+    //             return;
+    //         }
+    //     });
+    
+    //     queryString = "INSERT INTO lista_recursos (cadeia_logis, recurso) VALUES (?,?)";
+    //     conn.query(queryString, [idcad, idrec], (err,result) => {
+    //         if (err) {
+    //             conn.release();
+
+    //             res.status(500);
+    //             res.type('json');
+    //             res.send({"message":"Não foi possível realizar essa operação. output 13"});
+    //             return;
+    //         }
+    //     });
+    
+    //     const quantpol = req.body.qtpol;
+    //     queryString = "INSERT INTO poluicao_cadeia (cadeia_logis, poluicao, quantidade) VALUES (?,?,?)";
+    //     conn.query(queryString, [idcad, idpol, quantpol], (err,result) => {
+    //         if (err) {
+    //             conn.release();
+
+    //             res.status(500);
+    //             res.type('json');
+    //             res.send({"message":"Não foi possível realizar essa operação. output 14"});
+    //             return;
+    //         }
+    //     });
+    
+    //     const prov = req.body.id;
+    //     const nome = req.body.nome;
+    //     const dataprod = req.body.dataprod;
+    //     const preco = req.body.preco;
+    //     const tipo = req.body.tipo;
+    //     const subtipo = req.body.subtipo;
+    //     queryString = "INSERT INTO produto (nome, fornecedor, producao, preco, tipo, subtipo, cadeia_logis) \
+    //                                 VALUES (?,?,?,?,?,?,?)";
+    //     conn.query(queryString, [nome, prov, dataprod, preco, tipo, subtipo, idcad], (err, result) => {
+    //         conn.release();
+            
+    //         if(!err){
+    //             res.status(200);
+    //             res.type('json');
+    //             res.send({"message":"Registo bem sucessido"});
+    //             return;
+    //         } else {
+    //             res.status(500);
+    //             res.type('json');
+    //             res.send({"message":"Não foi possível realizar essa operação. output 15"});
+    //             return;
+    //         }
+    //     });
+    // });
 });
 
 router.get('/orders/:pid', (req,res) => {
