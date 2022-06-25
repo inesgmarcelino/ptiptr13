6db10a75dff9d055421484894e08bbe21ca68507
inesgmarcelino <inesgmarcelino@gmail.com>
2022-06-25 19:58:59 +0100

    transporters

--- ecomarket-app/ecomarket-backend/routes/api/v1/transporters.js
+++ ecomarket-app/ecomarket-backend/routes/api/v1/transporters.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 // https://stackoverflow.com/questions/62134713/nodejs-mysql-connection-best-practice
 // https://mhagemann.medium.com/create-a-mysql-database-middleware-with-node-js-8-and-async-await-6984a09d49f4
-// var pool = require('../svlib/db/getPool');
-var pool = require('../svlib/db/connection');
+var pool = require('../svlib/db/getPool');
 
 
 /** auth0 */
