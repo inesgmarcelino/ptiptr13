5a7959d1c344fd1d9e37e6cae815694c8fa1f096
fc51601 <fc51601@alunos.fc.ul.pt>
2022-06-25 05:56:02 +0100

    Update async.js

--- ecomarket-app/ecomarket-backend/routes/api/v0/async.js
+++ ecomarket-app/ecomarket-backend/routes/api/v0/async.js
@@ -12,11 +12,11 @@ const promisePool = pool.promise()
 exports.teste = async function(req,res){
     try{
         console.log("First query");
-        const insert = await promisePool.execute("INSERT INTO us(value) VALUES (?)",["primeiro"]);
+        const insert = await promisePool.query("INSERT INTO us(value) VALUES (?)",["primeiro"]);
         console.log("2 query");
-        const select = await promisePool.execute("SELECT MAX(id) AS id FROM us");
+        const select = await promisePool.query("SELECT MAX(id) AS id FROM us");
         console.log("3 query");
-        const update = await promisePool.execute("UPDATE us SET value = ? WHERE id = ?",["o valor nao eh primeiro",select.id]);
+        const update = await promisePool.query("UPDATE us SET value = ? WHERE id = ?",["o valor nao eh primeiro",select.id]);
         console.log("END");
     } catch(err) {
         console.log(err);
