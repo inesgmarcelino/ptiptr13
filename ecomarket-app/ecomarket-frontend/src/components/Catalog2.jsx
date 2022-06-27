ff1c20bacd24e14367fa871c31a94af3f2e9b229
sofiap3 <74013758+sofiap3@users.noreply.github.com>
2022-06-28 00:34:56 +0100

    .

--- ecomarket-app/ecomarket-frontend/src/components/Catalog2.jsx
+++ ecomarket-app/ecomarket-frontend/src/components/Catalog2.jsx
@@ -7,24 +7,21 @@ function Catalog2(){
                     <div className='col-sm-3'>
 
                     </div>
+                    
                     <div className='col-sm-9'>
-                    <div class="product-container">
-            <div class="d-flex flex-row">
-                <div class="product-card">
-                    <div class="product-info">
-                        <h2 class="product-name">brand</h2>
-                        <p class="product-short-des">cereais</p>
-                        <span class="price">2€</span>
-                    </div>
-                </div>
-            
-                <div class="product-info">
-                    <h2 class="product-name">continente</h2>
-                    <p class="product-short-des">cereais</p>
-                    <span class="price">1,5€</span>
-                </div>
-            </div>
-        </div>
+                        <div class="product-container">
+                            <div class="d-flex flex-row">
+                                <div class="product-card">
+                                    <div class="product-info">
+                                        <h2 class="product-name">brand</h2>
+                                        <p class="product-short-des">cereais</p>
+                                        <span class="price">2€</span>
+                                    </div>
+                                </div>
+
+                
+                            </div>
+                        </div>
                     </div>
 
                 </div>
