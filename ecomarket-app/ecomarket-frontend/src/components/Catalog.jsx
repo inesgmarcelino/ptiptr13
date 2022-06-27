180d35edcd3c1c23955b201f6a3673f748df6328
inesgmarcelino <inesgmarcelino@gmail.com>
2022-06-27 22:44:05 +0100

    catalog

--- ecomarket-app/ecomarket-frontend/src/components/Catalog.jsx
+++ ecomarket-app/ecomarket-frontend/src/components/Catalog.jsx
@@ -1,32 +1,50 @@
-const img1 =  require('../images/cereaisGoldenGraham.jpg');
-const img2 = require('../images/cereaisChocolateNegro.jpg')
+import React from 'react';
+import Axios from "axios";
+
 function Album(){
+    const tp = 3;
+    const st = 3;
+    document.body.onload = function(){};
+
+    const prods = () => {
+        Axios.get("https://ecomarket.works/api/products", {
+            params: {
+                tipo: tp,
+                subtipo: st
+            }
+        }).then((response) => {
+            if (response.data.message !== 'fail') {
+                var lista = response.data.results;
+                var i = Math.floor(lista.length/3);
+                var j = lista.length%3;
+
+                for (var k = 0; k < lista; k++) {
+                    while 
+                }
+            }
+        })
+    }
+
+
     return(
         <div class="product-container">
-            <div class="product-card">
-                <div class="product-image">
-                    <img src={img1} class="product-thumb" alt="" />
+            <div className="row mx-5"> 
+                <div className="card mb-3">            
+                    <div class="product-info">
+                        <h2 class="product-name">brand</h2>
+                        <p class="product-short-des">cereais</p>
+                        <span class="price">2€</span>
+                    </div>
                 </div>
-            
-                <div class="product-info">
-                    <h2 class="product-name">brand</h2>
-                    <p class="product-short-des">cereais</p>
-                    <span class="price">2€</span>
-                </div>
-            </div>
 
-            <div class="product-card">
-                <div class="product-image">
-                    <img src={img2} class="product-thumb" alt="" />
+                <div className="card mb-3">            
+                    <div class="product-info">
+                        <h2 class="product-name">continente</h2>
+                        <p class="product-short-des">cereais</p>
+                        <span class="price">1,5€</span>
+                    </div>
                 </div>
-            
-                <div class="product-info">
-                    <h2 class="product-name">continente</h2>
-                    <p class="product-short-des">cereais</p>
-                    <span class="price">1,5€</span>
-                </div>
-            </div>
-
+            </div> 
 
         
 
