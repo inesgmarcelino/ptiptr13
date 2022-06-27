f0d1363fcc1b5dbe1d2b5df931fe41ea42366cb4
fc51601 <fc51601@alunos.fc.ul.pt>
2022-06-28 00:38:24 +0100

    Update users.js

--- ecomarket-app/ecomarket-backend/routes/api/v1/users.js
+++ ecomarket-app/ecomarket-backend/routes/api/v1/users.js
@@ -49,6 +49,7 @@ router.post('/register', (req, res, next) => {
             if (req.body.trans) {
                 const address = encodeURIComponent(req.body.morada);
                 //address = address.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+                console.log(address);
                 const location = await axios.post("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyAo6Nzo6UBDA2oEHjWeCAFfVqfEq-2-0S4&language=pt");
                 if (location.results.status === udndefined || location.results.status !== "OK") throw new Error("Location Invalid");
                 var parts = {};
