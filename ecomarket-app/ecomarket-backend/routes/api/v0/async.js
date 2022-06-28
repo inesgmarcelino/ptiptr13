128a7c0b9fc866ccce0c0e50c5ed9abdd77bc181
fc51601 <fc51601@alunos.fc.ul.pt>
2022-06-28 03:26:08 +0100

    Update async.js

--- ecomarket-app/ecomarket-backend/routes/api/v0/async.js
+++ ecomarket-app/ecomarket-backend/routes/api/v0/async.js
@@ -13,14 +13,13 @@ exports.teste = async function (req, res) {
     
     try {
         const [results,fields] = await pool.query("SELECT id, morada AS addr FROM utilizador WHERE id = 15");
-        console.log(results);
         const person = results[0];
         const address = encodeURIComponent(person.addr);
         //address = address.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
         console.log(address);
         const link = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyAo6Nzo6UBDA2oEHjWeCAFfVqfEq-2-0S4&language=pt";
-        const result = await axios.post(link).then((response) => {console.log(response)}).catch((err) => {throw err});
-
+        const result = await axios.post(link).then((response) => {console.log(response.data)}).catch((err) => {throw err});
+        
 
 
         res.send(result);
