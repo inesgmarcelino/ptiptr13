f30708b6a47e50ea020fb423b6759410e1be4c6a
fc51601 <fc51601@alunos.fc.ul.pt>
2022-06-28 03:18:08 +0100

    Update async.js

--- ecomarket-app/ecomarket-backend/routes/api/v0/async.js
+++ ecomarket-app/ecomarket-backend/routes/api/v0/async.js
@@ -19,7 +19,7 @@ exports.teste = async function (req, res) {
         //address = address.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
         console.log(address);
         const link = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyAo6Nzo6UBDA2oEHjWeCAFfVqfEq-2-0S4&language=pt";
-        const result = await axios.post(link);
+        const result = await axios.post(link).then((response) => {console.log(response)}).catch((err) => {throw err});
 
 
 
