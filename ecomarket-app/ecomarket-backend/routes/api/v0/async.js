6feefb174584430d5e18f934bdbd60c7b2e00929
fc51601 <fc51601@alunos.fc.ul.pt>
2022-06-28 03:17:11 +0100

    Update async.js

--- ecomarket-app/ecomarket-backend/routes/api/v0/async.js
+++ ecomarket-app/ecomarket-backend/routes/api/v0/async.js
@@ -6,10 +6,7 @@ const pool = require('mysql2').createPool({
     password: "Y1nGJ14Ng#",
     database: "ecodb",
     port: 6033
-  });
-
-  const promisePool = pool.promise()
-
+  }).promise();
 
 
 exports.teste = async function (req, res) {
@@ -22,14 +19,11 @@ exports.teste = async function (req, res) {
         //address = address.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
         console.log(address);
         const link = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyAo6Nzo6UBDA2oEHjWeCAFfVqfEq-2-0S4&language=pt";
-        axios.post(link).then(async (response) => {
-            console.log(response);
-        }
-
-        );
+        const result = await axios.post(link);
 
 
-        res.send("OH  YES");
+
+        res.send(result);
     } catch (err) {
         console.log(err);
         res.send("OH  NOE");
