248230ae515937fc4f58f55dcfa1777e7847d490
fc51601 <fc51601@alunos.fc.ul.pt>
2022-07-07 05:40:16 +0100

    consumers and transporters half implemented

--- ecomarket-app/ecomarket-backend/routes/api/v2/consumers.js
+++ ecomarket-app/ecomarket-backend/routes/api/v2/consumers.js
@@ -4,17 +4,63 @@ const parser = require('../svlib/validator/parser');
 const ServerError = require('../svlib/ServError/ServerError');
 const pool = require('../svlib/db/getPool');
 
-router.get('/:uid/cart', async (req,res,next) => {
+function IsConsumidor(user){
+    const query = "SELECT * FROM utilizador WHERE id = ?";
+    const [result,garbage] = await pool.query(query,[user]).catch(err =>{
+        console.error(err);
+        throw new ServerError(500,"Não foi possível concretizar ação pedida.\nTente mais tarde.");
+    });
+    if(result.length === 0) throw new ServerError(404,"Utilizador não foi encontrado.");
+    return (result[0].papel === 2 || result[0].papel === 4);
+}
 
+router.get('/:uid/cart', async (req,res,next) => {
+    const e_params = [1,{uid:{type:"number"}}];
+    try {
+        if (!parser(req.params, e_params)) throw new ServerError(400,"Dados fornecidos inválidos.");
+
+        //cesto_compras prod references stock id. stock id references prod
+        //fqtty is the quantity of product seller has
+        const query = "SELECT p.nome AS nome, s.forn AS forn, s.store AS store, s.preco AS preco, c.qtty AS qtty, s.qtty AS fqtty "
+                      "FROM cesto_compras c, stock s, produto p WHERE c.cons = ? AND "+
+                      "s.forn = c.forn AND s.id = c.prod AND "+
+                      "";
+        const [results,garbage] = await pool.query(query,[req.params.uid]).catch(err => {
+            console.error(err);
+            throw new ServerError(500,"Não foi possível concretizar ação pedida.\nTente mais tarde.");
+        });
+
+        results.forEach(e => {
+            //verify if quantity is right
+        });
+
+        res.send(200).send();
+    } catch (err) {
+        res.status(err.code).send(err.message);
+    }
+    
 });
 
 router.post('/:uid/cart', async (req,res,next)=>{
+    const e_params = [1,{uid:{type:"number"}}];
+    const e_body = [1,{uid:{type:"number"}}];
+    try {
+        if (!parser(req.params, e_params)) throw new ServerError(400,"Dados fornecidos inválidos.");
+        if (!parser(req.body, e_body)) throw new ServerError(400,"Dados fornecidos inválidos");
+    } catch (err) {
+        res.status(err.code).send(err.message);
+    }
 
-})
+});
 
 router.post('/order', async (req,res,next) => {
-
-
+    const e_body = [1,{uid:{type:"number"}}];
+    try {
+        if (!parser(req.body, e_body)) throw new ServerError(400,"Dados fornecidos inválidos");
+        
+    } catch (err) {
+        res.status(err.code).send(err.message);
+    }
 
 });
 
--- ecomarket-app/ecomarket-backend/routes/api/v2/transporters.js
+++ ecomarket-app/ecomarket-backend/routes/api/v2/transporters.js
@@ -4,12 +4,51 @@ const parser = require('../svlib/validator/parser');
 const ServerError = require('../svlib/ServError/ServerError');
 const pool = require('../svlib/db/getPool');
 
-router.post('/:tid/vehicles/register', async (req,res,next) => {
+function IsTransportador(user){
+    const query = "SELECT * FROM utilizador WHERE id = ?";
+    const [result,garbage] = await pool.query(query,[user]).catch(err =>{
+        console.error(err);
+        throw new ServerError(500,"Não foi possível concretizar ação pedida.\nTente mais tarde.");
+    });
+    if(result.length === 0) throw new ServerError(404,"Utilizador não foi encontrado.");
+    return (result[0].papel === 5);
+}
 
+router.post('/:tid/vehicles/register', async (req,res,next) => {
+    const e_params = [1,{tid:{type:"number",min:1}}];
+    const e_body = [5,{
+        marca:{},
+        ano:{},
+        fuel:{},
+        plate:{}
+    }];
+    try {
+        if (!parser(req.params, e_params)) throw new ServerError(400,"Dados fornecidos inválidos.");
+        if (!parser(req.body, e_body)) throw new ServerError(400,"Dados fornecidos inválidos");
+        if(!IsTransportador(req.params.tid)) throw new ServerError(403,"");
+        const query ="INSERT INTO veiculo(transp,marca,ano,fuel,consumo,plate) VALUES (?,?,?,?,?)";
+        const params = [
+            req.params.tid, req.body.marca,req.body.ano,req.body.fuel,req.body.consu,req.body.plate
+        ]
+        const [results,garbage] = await pool.query(query,params).catch(err => {
+            console.error(err);
+            throw new ServerError(500,"Não foi possível concretizar ação pedida.\nTente mais tarde.");
+        });
+
+        res.status(200).send("O veiculo foi registado com sucesso");
+    } catch (err) {
+        res.status(err.code).send(err.message);
+    }
 });
 
 router.get('/:tid/vehicles', async (req,res,next) => {
-
+    const e_params = [1,{tid:{type:"number",min:1}}];
+    try {
+        if (!parser(req.params, e_params)) throw new ServerError(400,"Dados fornecidos inválidos.");
+        const query = "SELECT "   
+    } catch (err) {
+        res.status(err.code).send(err.message);       
+    }
 });
 
 router.get(':/tid/vehicles/:vid', async (req,res,next) => {
